b05ea730834e1e5860a40eb66b5a79bcde0320ad
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2024-02-08 21:22:14 +0200

    lay Out V1

--- Warehouses/Warehouses/Program.cs
+++ Warehouses/Warehouses/Program.cs
@@ -16,7 +16,7 @@ namespace Warehouses
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Ordersform());
         }
     }
 }
