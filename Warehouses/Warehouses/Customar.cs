3df8d7730958ea013c2ff720f2bb7dbb56f0072c
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2024-02-09 18:18:45 +0200

    Data Base Creation
    Code First

---
+++ Warehouses/Warehouses/Customar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouses
+{
+    internal class Customar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
---
+++ Warehouses/Warehouses/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouses
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime date { get; set; }
+        public string State { get; set; }
+        public Transaction transaction { get; set; }
+        public Delivery delivery { get; set; }
+        public Customar customar { get; set; }
+        public Supplier supplier { get; set; }
+        public List<OrderItem> Orderitems { get; set; }
+        public List<Inventory> Inventorys { get; set; }
+
+
+    }
+}
