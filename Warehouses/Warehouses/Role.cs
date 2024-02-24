184f8446380907072ca7f24e538c108cd9d1e428
Islam abu el-soud <100817976+Islam-abuelsoud@users.noreply.github.com>
2024-02-24 02:14:34 +0200

    ..

--- Warehouses/Warehouses/Role.cs
+++ Warehouses/Warehouses/Role.cs
@@ -26,6 +26,7 @@ namespace Warehouses
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Type { get; set; }
+        public string Password { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Order> Orders { get; set; }
